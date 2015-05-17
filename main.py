392cbd7280ffc64461dffcc060b9d593b2609a29
omarayad1 <omarayad1@gmail.com>
2015-05-17 11:20:05 +0200

    fixed links, moved them to static

--- main.py
+++ main.py
@@ -5,5 +5,18 @@ app = Flask(__name__)
 def render_public_site():
     return render_template('public.html')
 
+@app.route("/")
+def render_login_site():
+    return render_template('login.html')
+
+@app.route("/signup")
+def render_signup_site():
+    return render_template('signup.html')
+
+
+@app.route("/dashboard")
+def render_dashboard_site():
+    return render_template('dashboard.html')
+
 if __name__ == "__main__":
     app.run()
